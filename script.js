fa4453ae6361f15043ea79590c61080694c8d6a2
ECgonzo <ethegonzo2003@gmail.com>
2024-11-03 12:05:25 -0800

    searchString function

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ console.log(calculateCircumference(5));
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+function searchString(character, str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === character) count++;
+    }
+    return count;
+}
+console.log(searchString("s", "mississippi")); 
+console.log(searchString("q", "mississippi"));
